a7a73ed4cd87f5ceac538ee274c62c6c59e16eb9
hainguyen1999 <nghainguyen99@gmail.com>
2022-06-04 22:22:23 +0700

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
   return (
     <>
-      <footer className="w-100 footer_section d-flex justify-content-center align-items-center">
-        <div className="container">
-          <p className="ftr_copyrights my-auto">
-            All Rights Reserved By{" "}
-            <span className="author">Mounir El bertouli</span> -{" "}
+      <footer className='w-100 footer_section d-flex justify-content-center align-items-center'>
+        <div className='container'>
+          <p className='ftr_copyrights my-auto'>
+            Facebook Based Website Generator{' '}
+            {/* <span className="author"></span> -{" "} */}
             {new Date().getFullYear()}
           </p>
         </div>
